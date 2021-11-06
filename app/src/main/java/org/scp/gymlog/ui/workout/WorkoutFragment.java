849e8d5189620c3a11f1f3a7bffc0ead4e3bbfa4
scopp <osc.car2@gmail.com>
2021-11-06 20:21:28 +0100

    Init commit

---
+++ app/src/main/java/org/scp/gymlog/ui/workout/WorkoutFragment.java
@@ -0,0 +1,45 @@
+package org.scp.gymlog.ui.workout;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import org.scp.gymlog.databinding.FragmentWorkoutBinding;
+
+public class WorkoutFragment extends Fragment {
+
+	private WorkoutViewModel workoutViewModel;
+	private FragmentWorkoutBinding binding;
+
+	public View onCreateView(@NonNull LayoutInflater inflater,
+	                         ViewGroup container, Bundle savedInstanceState) {
+		workoutViewModel =
+				new ViewModelProvider(this).get(WorkoutViewModel.class);
+
+		binding = FragmentWorkoutBinding.inflate(inflater, container, false);
+		View root = binding.getRoot();
+
+		final TextView textView = binding.textWorkout;
+		workoutViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+			@Override
+			public void onChanged(@Nullable String s) {
+				textView.setText(s);
+			}
+		});
+		return root;
+	}
+
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+		binding = null;
+	}
+}
