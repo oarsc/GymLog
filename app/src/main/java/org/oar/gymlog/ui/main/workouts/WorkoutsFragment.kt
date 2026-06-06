ef1af33c3485e667de0185df41f1dbe7999f950c
oarsc <osc.car2@gmail.com>
2026-06-06 19:48:27 +0200

    Add workout model

---
+++ app/src/main/java/org/oar/gymlog/ui/main/workouts/WorkoutsFragment.kt
@@ -0,0 +1,83 @@
+package org.oar.gymlog.ui.main.workouts
+
+import android.content.Intent
+import android.content.res.ColorStateList
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.oar.gymlog.R
+import org.oar.gymlog.databinding.FragmentListWorkoutsBinding
+import org.oar.gymlog.databinding.ListitemWorkoutBinding
+import org.oar.gymlog.model.Workout
+import org.oar.gymlog.ui.LoadActivity
+import org.oar.gymlog.ui.common.components.listView.CommonListView
+import org.oar.gymlog.ui.common.components.listView.SimpleListHandler
+import org.oar.gymlog.util.Data
+
+/**
+ * A fragment representing a list of Items.
+ */
+class WorkoutsFragment : Fragment() {
+/**
+ * Mandatory empty constructor for the fragment manager to instantiate the
+ * fragment (e.g. upon screen orientation changes).
+ */
+
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View = FragmentListWorkoutsBinding.inflate(inflater, container, false)
+		.apply {
+			toolbar.setNavigationOnClickListener {
+				requireActivity().onBackPressedDispatcher.onBackPressed()
+			}
+
+			workoutsList.init(Data.workouts, WorkoutListHandler())
+
+			toolbar.setOnMenuItemClickListener { item: MenuItem ->
+				when (item.itemId) {
+					R.id.testButton -> {
+						requireActivity().apply {
+							val intent = Intent(this, LoadActivity::class.java)
+							intent.action = "keep"
+							startActivity(intent)
+						}
+//                        val seconds = 10
+//                        val date = NOW.plusSeconds(seconds.toLong())
+//                        NotificationService(requireContext())
+//                            .startNewNotification(date, seconds, Data.exercises[0].variations[0])
+					}
+				}
+				false
+			}
+		}
+		.root
+
+
+	inner class WorkoutListHandler : SimpleListHandler<Workout, ListitemWorkoutBinding> {
+		override val useListState = false
+		override val itemInflater: (LayoutInflater, ViewGroup?, Boolean) -> ListitemWorkoutBinding
+				= ListitemWorkoutBinding::inflate
+
+		override fun buildListView(
+			binding: ListitemWorkoutBinding,
+			item: Workout,
+			index: Int,
+			state: CommonListView.ListElementState?
+		) {
+			binding.apply {
+//				root.setOnClickListener { onMuscleClicked(item) }
+                content.text = item.name
+				image.setImageResource(R.drawable.ic_workout_black_24dp)
+				image.imageTintList = ColorStateList.valueOf(
+					resources.getColor(android.R.color.white, null)
+				)
+				//indicator.setBackgroundResource(android.R.color.white)
+			}
+		}
+	}
+}
